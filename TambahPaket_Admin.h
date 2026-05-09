9982e5e05259a7ae3f451ab5ab0d458097b7dec6
attar-code <attar.studycode@gmail.com>
2026-05-09 13:57:29 +0800

    anjay

--- TambahPaket_Admin.h
+++ TambahPaket_Admin.h
@@ -2,10 +2,10 @@
 #define TambahPaketAdmin_h
 #include "data.h"
 
-void TambahPaketAdmin(Paket DaftarPaket[], int &jumlahPaket);
-void LihatRiwayat(Paket DaftarPaket[], int jumlahPaket);
+void TambahPaketAdmin();
+void tampilRiwayatPaket();
 void AntriandanUpdateStatus(Paket DaftarPaket[], int jumlahPaket);
-void HapusRiwayatPaket(Paket DaftarPaket[], int &jumlahPaket);
+void HapusRiwayatPaket_Admin();
 void BatalkanPaket(Paket DaftarPaket[], int &jumlahPaket);
 
 #endif
