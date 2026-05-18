943ce2e65ae63d679eb95e8ee27daf1a7b53d43b
attar-code <attar.studycode@gmail.com>
2026-05-18 19:13:09 +0800

    masih nyoba

--- code/admin/LIhatAntriandanUpdate_Admin.cpp
+++ code/admin/LIhatAntriandanUpdate_Admin.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <cctype>
+#include <fstream>
 #include "../include/data.h"
 #include "../include/admin.h"
 #include "../database/json.hpp"
 
 using json = nlohmann::json;
 using namespace std;
+
+extern Paket paket[100];
+extern int jumlahPaket;
 
 bool paketdiproses(string status) {
     return (
@@ -106,6 +110,54 @@ void bubbleSortStatus(){
 
     }
 
+}
+
+void loadPaket(){
+
+    ifstream file("database/paket.json");
+    json j;
+    file >> j;
+
+    jumlahPaket = j.size();
+
+    for(int i = 0; i < jumlahPaket; i++){
+
+        paket[i].resi = j[i]["resi"];
+        paket[i].status = j[i]["status"];
+        paket[i].namaPengirim = j[i]["namaPengirim"];
+        paket[i].namaPenerima = j[i]["namaPenerima"];
+        paket[i].alamat = j[i]["alamat"];
+        paket[i].berat = j[i]["berat"];
+        paket[i].tipe = j[i]["tipe"];
+        paket[i].pembayaran = j[i]["pembayaran"];
+        paket[i].ongkir = j[i]["ongkir"];
+        paket[i].pemilik = j[i]["pemilik"];
+    }
+}
+
+void savePaket(){
+ 
+    json j = json::array();
+
+    for(int i = 0; i < jumlahPaket; i++){
+
+        j.push_back({
+            {"resi", paket[i].resi},
+            {"namaPengirim", paket[i].namaPengirim},
+            {"namaPenerima", paket[i].namaPenerima},
+            {"alamat", paket[i].alamat},
+            {"berat", paket[i].berat},
+            {"tipe", paket[i].tipe},
+            {"pembayaran", paket[i].pembayaran},
+            {"ongkir", paket[i].ongkir},
+            {"pemilik", paket[i].pemilik},
+            {"status", paket[i].status}
+        });
+    }
+
+    ofstream file("database/paket.json");
+    file << j.dump(4);
+    file.close();
 }
 
 void AntriandanUpdateStatus() {
@@ -227,9 +279,7 @@ void AntriandanUpdateStatus() {
                 }
             }
 
-            // ===============================
-            // 2. DIPROSES → DIKIRIM
-            // ===============================
+            // diproses -> dikirim
             else if (paketdiproses((ptr + i)->status)) {
 
                 (ptr + i)->status = "Dikirim";
@@ -237,9 +287,7 @@ void AntriandanUpdateStatus() {
                 cout << "\nStatus berubah: DIPROSES -> DIKIRIM\n";
             }
 
-            // ===============================
-            // 3. DIKIRIM → SELESAI
-            // ===============================
+            /// dikirim -> selesai
             else if ((ptr + i)->status == "Dikirim") {
 
                 (ptr + i)->status = "Selesai";
